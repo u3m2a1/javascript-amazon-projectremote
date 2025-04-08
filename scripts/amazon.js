6697dc026efd2830a008406dcfd4aeefa89ee3c3
u3m2a1 <umakallepally6543@gmail.com>
2025-04-08 21:42:48 +0530

    Reuse the function calculateCartQuantity from cart.js to checkout.js and amazon.js using module

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart, addToCart } from "../data/cart.js";
+import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/product.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -62,13 +62,11 @@ console.log(productsHTML);
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 function updateCartQuantity() {
-  let cartQuantity = 0;
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
-
+  const cartQuantity = calculateCartQuantity();
   document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 }
+
+updateCartQuantity();
 
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
@@ -80,3 +78,5 @@ document.querySelectorAll('.js-add-to-cart')
       
     });
   });
+
+
