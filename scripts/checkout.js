8eb5a396a4627a5217d19d74ced3281c89c5c8c5
u3m2a1 <umakallepally6543@gmail.com>
2025-04-22 20:59:50 +0530

    run both loadProductsFetch and loadCartFetch at a time in checkout.js using promise.all

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,7 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadProducts, loadProductsFetch } from "../data/product.js";
-import { loadCart } from "../data/cart.js";
+import { loadCart, loadCartFetch } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/car.js';
 // import '../data/backend-practice.js';
@@ -10,18 +10,10 @@ import { loadCart } from "../data/cart.js";
 
 async function loadPage() {
   try{
-    // throw 'error1';
-
-    await loadProductsFetch();
-
-    const value = await new Promise((resolve, reject) => {
-      // throw 'error2';
-      loadCart(() => {
-        // reject('error3');
-        resolve('value3');
-      });
-    });
-
+   await Promise.all([
+      loadProductsFetch(),
+      loadCartFetch()
+    ]);
   } catch (error) {
     console.log('Unexpected error. Please try again later.');
   }
